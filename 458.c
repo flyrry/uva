ba13861a8d890d853de8b7838c2c9f80467a6db9
Sergei Mironenko <sergeimail@gmail.com>
2012-03-22 22:37:54 +0200

    super fast 458

--- 458.c
+++ 458.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
+
+#define IO_BUFFER_SIZE 1048576
 
 int main(void) {
-  register char c;
-  while ((c = getchar()) != EOF) {
-    putchar((c == '\n') ? c : c - 7);
+  register int sz, i;
+  char in[IO_BUFFER_SIZE], out[IO_BUFFER_SIZE];
+  while ((sz = read(0, in, IO_BUFFER_SIZE)) > 0) {
+    for (i = 0; i < sz; ++i)
+      out[i] = (in[i] == '\n') ? in[i] : in[i] - 7;
+    write(1, out, sz);
   }
   return 0;
 }

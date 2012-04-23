c57bf02dd29a015b5c60258c3aa8f88939a3c5af
Sergei Mironenko <sergeimail@gmail.com>
2012-04-23 10:38:18 +0300

    793 - Network Connections switched from graph filling to disjoint sets

--- 793.cpp
+++ 793.cpp
@@ -1,150 +1,63 @@
 #include <iostream>
-#include <map>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <queue>
 
 using namespace std;
 
-typedef int vdata;
+#define MAX_COMPUTERS 100000
 
-struct Vertex 
-{
-  vdata id;
-  int color;
-  vector<Vertex*> adj;
-  Vertex (vdata id) : id(id), color(-1) {}
-};
+int parent[MAX_COMPUTERS];
+int rank[MAX_COMPUTERS];
 
-typedef map<vdata, Vertex*> vmap;
-typedef pair<vdata, Vertex*> vpair;
-
-class Graph 
-{
-  public:
-    Graph() {}
-    ~Graph();
-    void fill();
-    Vertex* addVertex (vdata v);
-    void addEdge (vdata begin, vdata end, bool directed);
-    Vertex* firstVertex();
-    Vertex* getVertex (vdata v);
-    vmap vertexMap;
-    vector<Vertex*> allVertexes;
-};
-
-Graph::~Graph()
-{
-  for (int i = 0; i < allVertexes.size(); ++i)
-    delete allVertexes[i];
+int find_root(int x) {
+  if (parent[x] != x)
+    parent[x] = find_root(parent[x]);
+  return parent[x];
 }
 
-Vertex* Graph::addVertex(vdata v) {
-  Vertex* newv = new Vertex(v);
-  allVertexes.push_back(newv);
-  vertexMap.insert(vpair(v, newv));
-  return newv;
-}
-
-void Graph::addEdge(vdata begin, vdata end, bool directed = false)
-{
-  Vertex* v = getVertex(begin);
-  Vertex* w = getVertex(end);
-  v->adj.push_back(w);
-  if (!directed)
-    w->adj.push_back(v);
-}
-
-Vertex* Graph::getVertex(vdata v)
-{
-  vmap::iterator it(vertexMap.find(v));
-
-  if (it == vertexMap.end()) {
-    return addVertex(v);
-  }
-
-  return (*it).second;
-}
-
-Vertex* Graph::firstVertex()
-{
-  return allVertexes[0];
-}
-
-void Graph::fill() {
-  queue<Vertex*> q;
-  Vertex* temp;
-  int color = 0;
-  for (int i = 0; i < allVertexes.size(); ++i) allVertexes[i]->color = -1;
-
-  for (int i = 0; i < allVertexes.size(); ++i) {
-    if (allVertexes[i]->color == -1) {
-      ++color;
-      while (!q.empty()) q.pop();
-
-      q.push(allVertexes[i]);
-      while (!q.empty()) {
-        temp = q.front();
-        q.pop();
-
-        temp->color = color;
-        for (int j = 0; j < temp->adj.size(); ++j) {
-          if (temp->adj[j]->color == -1) q.push(temp->adj[j]);
-          temp->adj[j]->color = color;
-        }
-      }
-    }
+void join_sets(int x, int y) {
+  int x_root = find_root(x);
+  int y_root = find_root(y);
+  if (x_root == y_root) return;
+  if (rank[x_root] > rank[y_root])
+    parent[y_root] = x_root;
+  else if (rank[y_root] = rank[x_root])
+    parent[x_root] = y_root;
+  else {
+    parent[x_root] = y_root;
+    ++rank[y_root];
   }
 }
 
 int main (void) {
   int cases;
   int computers;
-  string input;
-  stringstream ss;
   cin >> cases;
   char command;
   int c1, c2;
-  Vertex* b;
-  Vertex* e;
   int suc_count, unsuc_count;
 
   while (cases--) {
-    Graph* g = new Graph();
     suc_count = unsuc_count = 0;
     cin >> computers;
 
     for (int i = 1; i <= computers; ++i)
-      g->addVertex(i);
+      parent[i] = i;
 
-    getline(cin,input);
-    while (getline(cin,input)) {
-      if (!input.size()) break;
-      ss << input;
-      ss >> command >> c1 >> c2;
-      ss.str(string());
-      ss.clear();
+    while (cin >> command >> c1 >> c2) {
+      cin.ignore(1000, '\n');
 
       if (command == 'c') {
-        g->addEdge(c1,c2);
+        join_sets(c1, c2);
       } else {
-        g->fill();
-        b = g->getVertex(c1);
-        e = g->getVertex(c2);
-
-        if (b->color == e->color) {
+        if (find_root(c1) == find_root(c2))
           ++suc_count;
-        } else {
+        else
           ++unsuc_count;
-        }
       }
+      if (cin.peek() == '\n') break;
     }
 
     cout << suc_count << "," << unsuc_count << endl;
     if (cases) cout << endl;
-
-    delete g;
   }
 
   return 0;
